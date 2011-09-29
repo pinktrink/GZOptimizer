8bcdee469ffe441ddd100ed08ce5dba84d81bf6e
Eric Kever <lolbummer@gmail.com>
2011-09-29 13:51:38 -0400

    Initial commit.

---
+++ gzoptimizer.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+#This goes in the apache preprocessing stack before the gzip handler. I currently have no friggin idea how to do this, because I suck.
+#This will optimize the html for better gzip compression.
+
+use strict;
+use warnings;
+
+my $data = '';
+my %replace;
+
+while(<>){
+	$data .= $_;
+}
+
+while($data =~ m/<((?<!\?)[a-z-]+)\s*(.*?)(\/?>)/ig){
+	my $tag = $1;
+	my $tags = $2;
+	my $end = $3;
+	my $full = $&;
+	my (%attrs, $fulltag);
+	
+	$end = ' />' if $end eq '/>';
+	
+	while($tags =~ m/([a-z-:]+)(?:=((['"]?).*?\3))?/ig){
+		my $attr = $1;
+		my $value = $2;
+		
+		$attrs{$attr} = $value;
+	}
+	
+	$fulltag = "<$tag";
+	foreach my $key (sort keys %attrs){
+		$fulltag .= " $key=" . $attrs{$key};
+	}
+	$fulltag .= $end;
+	
+	$replace{$full} = $fulltag;
+}
+
+while (my ($k, $v) = each %replace){
+	$data =~ s/$k/$v/;
+}
+
+print $data;
+
+__END__
+GOOD JOB!
